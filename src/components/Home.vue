7f5b5fcea32d4a1b5c437c6e7d033439aac5e73f
bestrui <bestrui@bestrui.top>
2023-12-22 13:09:30 +0000

    首页

---
+++ src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="join join-vertical w-full">
+        <div class="join-item border items-center border-base-300">
+            <div style="text-align: center;" class="collapse-title  text-xl font-medium">
+                请选择兵种
+            </div>
+
+        </div>
+    </div>
+    <div class="chosemod flex  justify-center">
+
+        <div class="mx-4 card w-96 bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="/bubing.png" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">步兵机器人</h2>
+                <div class="card-actions">
+                    <button class="btn btn-primary">选择</button>
+                </div>
+            </div>
+        </div>
+        <div class="mx-4card w-96 bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="/bubing.png" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">英雄机器人</h2>
+                <div class="card-actions">
+                    <button class="btn btn-primary">选择</button>
+                </div>
+            </div>
+        </div>
+        <div class="mx-4 card w-96 bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="/bubing.png" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">哨兵机器人</h2>
+                <div class="card-actions">
+                    <button class="btn btn-primary">选择</button>
+                </div>
+            </div>
+        </div>
+        <div class="mx-4 card w-96 bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="/bubing.png" alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">工程机器人</h2>
+                <div class="card-actions">
+                    <button class="btn btn-primary">选择</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="fixed bottom-8 left-0 right-0 flex justify-center">
+        <ul class="mb-4 steps mt-8">
+            <li class="step step-primary">选择兵种</li>
+            <li class="step">第二步</li>
+            <li class="step">第三步</li>
+            <li class="step">第四步</li>
+        </ul>
+    </div>
+</template>
+<style></style>
